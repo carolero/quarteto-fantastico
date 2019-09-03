eed96b5330e9172ed156db67a799152a6a27f62b
Carol <carolina.oliveira@zup.com.br>
2019-09-03 17:50:42 -0300

    Iniciar projeto

---
+++ src/main/java/com/br/zuplardario/QuartetoFantasticoApplication.java
@@ -0,0 +1,13 @@
+package com.br.zuplardario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartetoFantasticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartetoFantasticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zuplardario/QuartetoFantasticoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.zuplardario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartetoFantasticoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
